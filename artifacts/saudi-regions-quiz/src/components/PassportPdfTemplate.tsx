4aa3736128bf7d1eb44074ff8c2e8a2a8783bab2
Mohanned Mahmoud <m.mahmoud2203@nu.edu.eg>
2026-09-21 23:35:51 +0300

    Use absolute pixel top positioning instead of transform for html2canvas compatibility

--- artifacts/saudi-regions-quiz/src/components/PassportPdfTemplate.tsx
+++ artifacts/saudi-regions-quiz/src/components/PassportPdfTemplate.tsx
@@ -24,7 +24,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-cover-hq.png"
               alt="Cover"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
           </div>
         </div>
@@ -35,7 +35,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-intro.svg"
               alt="Intro"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
             
             <div style={{ position: 'absolute', top: '28%', left: '3.5%', width: '40%', height: '6.64%', display: 'flex', alignItems: 'center', paddingLeft: '2%', paddingRight: '2%', color: '#004D40', fontSize: '22px', fontWeight: 'bold', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
@@ -56,7 +56,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-intro.svg"
               alt="Intro"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
           </div>
         </div>
@@ -67,7 +67,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-page-hq.png"
               alt="Pages"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
 
             {regions.map((region) => {
@@ -106,7 +106,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-page-hq.png"
               alt="Pages"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
 
             {regions.map((region) => {
@@ -145,7 +145,7 @@ export const PassportPdfTemplate = forwardRef<HTMLDivElement, PassportPdfTemplat
             <img
               src="/passport-cover-hq.png"
               alt="Cover"
-              style={{ position: 'absolute', top: '50%', left: '50%', width: '1440px', height: '1422.2px', transform: 'translate(-50%, -50%)' }}
+              style={{ position: 'absolute', top: '-199px', left: 0, width: '1440px', height: '1422px' }}
             />
           </div>
         </div>
